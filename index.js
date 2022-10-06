faabef0a9a46fa6349953870894f2ef6ddba254e
Евгения Амелина <shkarina.evgenia@yandex.ru>
2022-10-06 19:12:28 +0300

    added webpack, babel, other

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // все ячейки поля
 const cells = document.querySelectorAll(".game__cell");
-const againButton = document.querySelector('.game__button');
+const againButton = document.querySelector(".game__button");
 
 console.log(cells);
 
@@ -24,60 +24,36 @@ function handleCellClick(cell) {
 }
 
 function handleButtonClick() {
-    console.log('click button')
+  console.log("click button");
   return cells.forEach((cell) => {
-        cell.innerText = '';
-        cell.style.backgroundColor = "aliceblue";
-        cell.removeAttribute('disabled');
-    });
+    cell.innerText = "";
+    cell.style.backgroundColor = "aliceblue";
+    cell.removeAttribute("disabled");
+  });
 }
 
 function checkForWin() {
   if (
-    (cells[0].innerText == "o" &&
-      cells[1].innerText == "o" &&
-      cells[2].innerText == "o") ||
-    (cells[3].innerText == "o" &&
-      cells[4].innerText == "o" &&
-      cells[5].innerText == "o") ||
-    (cells[6].innerText == "o" &&
-      cells[7].innerText == "o" &&
-      cells[8].innerText == "o") ||
-    (cells[0].innerText == "o" &&
-      cells[4].innerText == "o" &&
-      cells[8].innerText == "o") ||
-    (cells[2].innerText == "o" &&
-      cells[4].innerText == "o" &&
-      cells[6].innerText == "o") ||
-    (cells[0].innerText == "+" &&
-      cells[1].innerText == "+" &&
-      cells[2].innerText == "+") ||
-    (cells[3].innerText == "+" &&
-      cells[4].innerText == "+" &&
-      cells[5].innerText == "+") ||
-    (cells[6].innerText == "+" &&
-      cells[7].innerText == "+" &&
-      cells[8].innerText == "+") ||
-    (cells[0].innerText == "+" &&
-      cells[4].innerText == "+" &&
-      cells[8].innerText == "+") ||
-    (cells[2].innerText == "+" &&
-      cells[4].innerText == "+" &&
-      cells[6].innerText == "+")
+    (cells[0].innerText == "o" && cells[1].innerText == "o" && cells[2].innerText == "o") ||
+    (cells[3].innerText == "o" && cells[4].innerText == "o" && cells[5].innerText == "o") ||
+    (cells[6].innerText == "o" && cells[7].innerText == "o" && cells[8].innerText == "o") ||
+    (cells[0].innerText == "o" && cells[4].innerText == "o" && cells[8].innerText == "o") ||
+    (cells[2].innerText == "o" && cells[4].innerText == "o" && cells[6].innerText == "o") ||
+    (cells[0].innerText == "+" && cells[1].innerText == "+" && cells[2].innerText == "+") ||
+    (cells[3].innerText == "+" && cells[4].innerText == "+" && cells[5].innerText == "+") ||
+    (cells[6].innerText == "+" && cells[7].innerText == "+" && cells[8].innerText == "+") ||
+    (cells[0].innerText == "+" && cells[4].innerText == "+" && cells[8].innerText == "+") ||
+    (cells[2].innerText == "+" && cells[4].innerText == "+" && cells[6].innerText == "+")
   ) {
     console.log("you won!");
     cells.forEach((cell) => {
-        cell.setAttribute('disabled', 'true');
-        cell.style.backgroundColor = "#86d986";
+      cell.setAttribute("disabled", "true");
+      cell.style.backgroundColor = "#86d986";
     });
   }
 }
 
-
-
 // слушатель по клику на каждую ячейку
-cells.forEach((cell) =>
-  cell.addEventListener("click", () => handleCellClick(cell))
-);
+cells.forEach((cell) => cell.addEventListener("click", () => handleCellClick(cell)));
 
-againButton.addEventListener('click', handleButtonClick);
+againButton.addEventListener("click", handleButtonClick);
